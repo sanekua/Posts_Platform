3fa4cd001e86125ddd511523cb6a8629dc46bde5
sanekua <sasha-kuzmyk@mail.ru>
2020-06-20 19:56:58 +0300

    create content posts

--- posts/posts/settings.py
+++ posts/posts/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'content.apps.ContentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'posts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'post',
+        'USER':'posts_app',
+        'PASSWORD':'post1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
