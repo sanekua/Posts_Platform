c345f807242774412a5ab9d7e662938b29b15a45
sanekua <sasha-kuzmyk@mail.ru>
2020-06-21 04:07:00 +0300

    html

--- posts/content/models.py
+++ posts/content/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
+    objects = models.Manager()
+    published = PublishedManager()
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
@@ -26,3 +33,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('content:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
+
+
